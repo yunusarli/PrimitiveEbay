0671307a75ded0532af26deb175d74da23b02e31
*Yunus <*yunusarlivdl@gmail.com*>
2021-06-04 13:50:45 +0300

    new features

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -11,9 +11,27 @@ class Listing(models.Model):
 
     createdby = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
     link = models.CharField(max_length=100,blank=True)
     category = models.CharField(max_length=50,default="Home")
     date = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("index")
+    
+
+
+class Bids(models.Model):
+    title = models.ForeignKey(Listing,on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.price)
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse("index")
+    
