c29b8db506f881f538eae4ddf458f0c283e8343a
*Yunus <*yunusarlivdl@gmail.com*>
2021-06-04 13:24:08 +0300

    added first features

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+
+    """ Listing class for creating listing model """
+
+    createdby = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    link = models.CharField(max_length=100,blank=True)
+    category = models.CharField(max_length=50,default="Home")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -25,7 +25,8 @@ def login_view(request):
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
+                "message": "Invalid username and/or password.",
+                
             })
     else:
         return render(request, "auctions/login.html")
